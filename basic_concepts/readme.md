57ed934287cff9ff8769eeb097fd2619180c4d9c
10530 <harshal.giri@neml.in>
2024-07-21 19:28:29 +0530

    initial commit

---
+++ basic_concepts/readme.md
@@ -0,0 +1,6 @@
+create table product(
+   pid int primary key,
+   pname varchar(50),
+   price float(10),
+   qty float(10)
+);
---
+++ basic_concepts/src/com/hibernate/concepts/LoadingData.java
@@ -0,0 +1,6 @@
+package com.hibernate.concepts;
+
+public class LoadingData {
+
+	// loading the data or record from database and storing it in entity class object
+}
